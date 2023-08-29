a866c193818ed52120061baacbf4fd42ae98cf17
BlueVelocity <josephrodney@hotmail.com>
2023-08-29 17:00:07 -0400

    Encapsulate javascript element selectors

--- script.js
+++ script.js
@@ -1,50 +1,52 @@
-const numberButtons = document.querySelectorAll('.button-number');
-const operatorButtons = document.querySelectorAll('.button-operator');
-const clearButton = document.getElementById('button-clear');
-const clearAllButton = document.getElementById('button-clear-all');
-const decimalButton = document.getElementById('button-decimal');
-const equalsButton = document.getElementById('button-equals');
-const displayCurrent = document.querySelector('.display-current');
-const displayPrevious = document.querySelector('.display-previous');
+const elements = {
+    numberButtons: document.querySelectorAll('.button-number'),
+    operatorButtons: document.querySelectorAll('.button-operator'),
+    clearButton:  document.getElementById('button-clear'),
+    clearAllButton: document.getElementById('button-clear-all'),
+    decimalButton: document.getElementById('button-decimal'),
+    equalsButton: document.getElementById('button-equals'),
+    displayCurrent: document.querySelector('.display-current'),
+    displayPrevious: document.querySelector('.display-previous')
+}
 
 const memory = {
     operantOne: "",
     operantTwo: "",
     leftoverOperant: false,
     decimalPresent: false,
-    operator: "",
+    operator: ""
 }
 
-numberButtons.forEach(numberElement => {
+elements.numberButtons.forEach(numberElement => {
     numberElement.addEventListener('click', () => addToOperant(numberElement));
 });
 
-operatorButtons.forEach(operatorElement => {
+elements.operatorButtons.forEach(operatorElement => {
     operatorElement.addEventListener('click', () => { 
-        if (memory.operator === "") {;
+        if (memory.operator === "") {
             outputToCurrentDisplay(operatorElement);
             commitOperator(operatorElement);
         }
     })
 });
 
-clearButton.addEventListener('click', clearCurrent);
+elements.clearButton.addEventListener('click', clearCurrent);
 
-clearAllButton.addEventListener('click', clearAll);
+elements.clearAllButton.addEventListener('click', clearAll);
 
-decimalButton.addEventListener('click', () => {
+elements.decimalButton.addEventListener('click', () => {
     if (memory.decimalPresent === false) {
-        addToOperant(decimalButton);
+        addToOperant(elements.decimalButton);
         memory.decimalPresent = true;
     }
 });
 
-equalsButton.addEventListener('click', equals);
+elements.equalsButton.addEventListener('click', equals);
 
 function equals() {
-    displayPrevious.textContent = displayCurrent.textContent;
-    displayCurrent.textContent = calculate(memory.operantOne, memory.operantTwo, memory.operator);
-    memory.operantOne = displayCurrent.textContent;
+    elements.displayPrevious.textContent = elements.displayCurrent.textContent;
+    elements.displayCurrent.textContent = calculate(memory.operantOne, memory.operantTwo, memory.operator);
+    memory.operantOne = elements.displayCurrent.textContent;
     memory.operantTwo = "";
     memory.operator = "";
     memory.leftoverOperant = true;
@@ -55,18 +57,18 @@ function clearCurrent() {
     memory.operantTwo = "";
     memory.operator = "";
     memory.decimalPresent = false;
-    displayCurrent.textContent = "";
+    elements.displayCurrent.textContent = "";
 }
 
 function clearAll() {
     clearCurrent();
-    displayPrevious.textContent = "";
+    elements.displayPrevious.textContent = "";
 }
 
 function clearLeftoverOperant() {
     if (memory.leftoverOperant === true) {
         memory.leftoverOperant = false;
-        displayCurrent.textContent = "";
+        elements.displayCurrent.textContent = "";
         memory.operantOne = "";
     }
 }
@@ -96,8 +98,8 @@ function commitOperator(operatorElement) {
 
 function outputToCurrentDisplay(element) {
     let inputData = element.getAttribute('data-input');
-    let displayCurrentText = displayCurrent.textContent;
-    displayCurrent.textContent = displayCurrentText + inputData;
+    let displayCurrentText = elements.displayCurrent.textContent;
+    elements.displayCurrent.textContent = displayCurrentText + inputData;
 }
 
 function calculate(operantOne, operantTwo, operator) {
