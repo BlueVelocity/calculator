91fe8aec66ee5d153aad4b5462daa3ca1475d1e3
BlueVelocity <josephrodney@hotmail.com>
2023-08-29 01:18:51 -0400

    Implement calculate function in console

--- script.js
+++ script.js
@@ -1,13 +1,30 @@
 let buttons = document.querySelectorAll('button');
 let displayCurrent = document.querySelector('.display-current');
 
+let operantOne = null;
+let operantTwo = null;
+let operator = null;
+
 buttons.forEach(element => element.addEventListener('click', () => {
-    addToMemory(element.getAttribute('data-input'))
+    outputToDisplay(element.getAttribute('data-input'))
 }));
 
-function addToMemory(elementDataInput) {
+function outputToDisplay(elementDataInput) {
     let displayCurrentText = displayCurrent.textContent;
     displayCurrent.textContent = displayCurrentText + elementDataInput
+}
+
+function calculate(operantOne, operantTwo, operator) {
+    switch(operator) {
+        case '+':
+            return add(operantOne, operantTwo);
+        case '-':
+            return subtract(operantOne, operantTwo);
+        case '*':
+            return multiply(operantOne, operantTwo);
+        case '/':
+            return divide(operantOne, operantTwo);
+    }
 }
 
 function add(num1, num2) {
